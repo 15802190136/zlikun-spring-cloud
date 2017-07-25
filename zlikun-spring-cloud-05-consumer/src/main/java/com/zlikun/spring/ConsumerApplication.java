76ff5a3d3ef7c0b0413b0a9219a6282a266e91be
zlikun <zlikun-dev@hotmail.com>
2017-07-25 19:55:10 +0800

    整合turbine，监控多个微服务实例

--- zlikun-spring-cloud-05-consumer/src/main/java/com/zlikun/spring/ConsumerApplication.java
+++ zlikun-spring-cloud-05-consumer/src/main/java/com/zlikun/spring/ConsumerApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -21,6 +22,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableHystrix
 @EnableCircuitBreaker
 @EnableHystrixDashboard
+@EnableTurbine
 public class ConsumerApplication {
 
     @Bean
