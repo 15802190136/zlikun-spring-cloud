3264c2a8c0805145389db3288f753431a71ee97c
zlikun <zlikun-dev@hotmail.com>
2017-07-25 19:46:30 +0800

    整合HystrixDashboard监控微服务

--- zlikun-spring-cloud-05-consumer/src/main/java/com/zlikun/spring/ConsumerApplication.java
+++ zlikun-spring-cloud-05-consumer/src/main/java/com/zlikun/spring/ConsumerApplication.java
@@ -2,10 +2,12 @@ package com.zlikun.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,6 +19,8 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableHystrix
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class ConsumerApplication {
 
     @Bean
